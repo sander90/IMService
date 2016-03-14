afebf23ac2179e7944d51464f89f9d8efdadd54e
sander90 <sander.shan@yahoo.com>
2016-03-14 16:52:56 +0800

    add project

---
+++ IMService/IMService.h
@@ -0,0 +1,13 @@
+//
+//  IMService.h
+//  IMService
+//
+//  Created by shansander on 16/3/13.
+//  Copyright © 2016年 shansander. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IMService : NSObject
++ (void)printdescription;
+@end
---
+++ IMService/IMService.m
@@ -0,0 +1,20 @@
+//
+//  IMService.m
+//  IMService
+//
+//  Created by shansander on 16/3/13.
+//  Copyright © 2016年 shansander. All rights reserved.
+//
+
+#import "IMService.h"
+
+@implementation IMService
+
++ (void)printdescription
+{
+    printf("#######\n");
+    printf("##测试##\n");
+    printf("#######\n");
+}
+
+@end
